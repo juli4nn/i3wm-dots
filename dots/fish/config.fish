d3d915363a259c4efd441978fe6a7664ef56d9d8
juli4nn <julianisnever@gmail.com>
2024-11-25 18:07:20 -0800

    dots

---
+++ dots/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc --pipe
+
+pfetch
+
+set fish_greeting
